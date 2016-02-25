613483e7606251401ba173ac131bb0ca0fd2cc76
Chris Jones <chris@pdxcodeguild.com>
2016-02-25 10:41:29 -0800

    fix typo again

--- testing/urls.py
+++ testing/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 import sitepages.views
-import sitepages.receivers
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
